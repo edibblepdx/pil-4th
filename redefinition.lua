766bf0f210aedd56a62f392c3fab77c680c956c9
ethan <ethanrdibble@gmail.com>
2024-09-16 14:56:50 -0700

    some lua exercises up through chapter 11

--- redefinition.lua
+++
@@ -1,9 +0,0 @@
-do
-    local oldSin = math.sin
-    local k = math.pi / 180
-    math.sin = function (x)
-        return oldSin(x * k)
-    end
-end
-
-print(math.sin(180))  -- first converts degrees to radians

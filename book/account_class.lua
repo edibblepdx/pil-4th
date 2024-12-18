85700e40392694b28780c33b40d672f6bed1bbf5
ethan dibble <edibble@pdx.edu>
2024-12-17 22:59:51 -0800

    chapter 21

---
+++ book/account_class.lua
@@ -0,0 +1,22 @@
+Account = {balance = 0}
+
+-- create a new Account object 
+-- and set Account as it's metatable
+-- Account:new (o) == Account.new(self, o)
+function Account:new (o)
+    o or {}
+    self.__index = self
+    setmetatable(o, self)
+    return o
+end
+
+-- increase an Account's balance
+function Account:deposit (v)
+    self.balance = self.balance + v
+end
+
+-- decrease an Account's balance
+function Account:withdraw (v)
+    if v > self.balance then error"insufficient funds" end
+    self.balance = self.balance - v
+end
